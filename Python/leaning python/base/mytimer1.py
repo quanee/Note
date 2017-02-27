0ea76bd6596f8a2442be9d21dba00af5317eafb7
quanee <262504455@qq.com>
2017-02-27 14:19:52 +0800

    update file mytimer1.py

--- Python/leaning python/base/mytimer1.py
+++ Python/leaning python/base/mytimer1.py
@@ -32,3 +32,11 @@ def timer(func, *pargs, **kargs):
     elapsed = timefunc() - start
     return (elapsed, ret)
 
+
+def best(func, *pargs, **kargs):
+    _reps = kargs.pop('_reps', 50)
+    best = 2 ** 32
+    for i in range(_reps):
+        (time, ret) = timer(func, *pargs, _reps=1, **kargs)
+        if time < best:
+            best = time
