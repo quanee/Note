d8f47793724e10565a7a6ddece3fed07989a736f
quanee <262504455@qq.com>
2017-02-27 14:19:49 +0800

    update file mytimer1.py

---
+++ Python/leaning python/base/mytimer1.py
@@ -0,0 +1,7 @@
+'''
+timer(spam, 1, 2, a=3, b=4, _reps=1000) calls and times spam(1, 2, a=3)
+_reps times, and returns tatal time for all runs,
+with final result;
+
+best(spam, 1, 2, a=3, b=4, _reps=50) runs best-of-N timer to filter out
+any system load variation, and returns best time among_reps tests
