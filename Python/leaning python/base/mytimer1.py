3643d3b3a88facecee8fe8c9dde7a1a953b0454a
quanee <262504455@qq.com>
2017-03-05 13:36:43 +0800

    update file mytimer1.py

--- Python/leaning python/base/mytimer1.py
+++ Python/leaning python/base/mytimer1.py
@@ -6,3 +6,6 @@ with final result;
 best(spam, 1, 2, a=3, b=4, _reps=50) runs best-of-N timer to filter out
 any system load variation, and returns best time among_reps tests
 '''
+
+import time
+import sys
