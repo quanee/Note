180204a2aaefbd16540c038b0edf7523f0aa60d4
quanee <262504455@qq.com>
2017-03-07 14:20:03 +0800

    update file asyncio_.py

--- Python/Threads_/asyncio_.py
+++ Python/Threads_/asyncio_.py
@@ -8,3 +8,7 @@ import asyncio
 
 
 @asyncio.coroutine
+def task(host, url='/'):
+    reader, writer = yield from asyncio.open_connection(host, 80)
+    request_header_content = '''GET %s HTTP/1.0\r\nHost: %s\r\n\r\n''' % (url, host, )
+    request_header_content = bytes(request_header_content, encoding='utf-8')
