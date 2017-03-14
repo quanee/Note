bbe0332058d30106a709f04d94cf3fa3504af87f
quanee <262504455@qq.com>
2017-03-14 14:20:11 +0800

    update file Python35.py

---
+++ Python/Python/Python35.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, Index
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://root:123@127.0.0.1:3306/t1", max_overflow=5)
+
+Base = declarative_base()
+
+# 操作表
+
+
+# 创建单表
+
+
+class Users(Base):
+    __tablename__ = 'users'
