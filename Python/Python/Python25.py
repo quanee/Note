c77fb10338d05c9d6b679ce9bd813e09defa16d7
quanee <262504455@qq.com>
2017-03-15 14:20:16 +0800

    update file Python25.py

--- Python/Python/Python25.py
+++ Python/Python/Python25.py
@@ -10,3 +10,15 @@ q.get([block[, timeout]]) 获取队列，timeout等待时间
 q.get_nowait() 相当q.get(False)
 非阻塞 q.put(item) 写入队列，timeout等待时间
 q.put_nowait(item) 相当q.put(item, False)
+q.task_done() 在完成一项工作之后，q.task_done() 函数向任务已经完成的队列发送一个信号
+q.join() 实际上意味着等到队列为空，再执行别的操作'''
+
+d = queue.Queue(3)  # 参数小于0 无限大
+
+d.put('a', 0)
+d.put('b')
+d.put('c')
+# d.put('c', 0)  # 队列满时 阻塞 参数0 报错
+
+print(d.get())
+print(d.get())
