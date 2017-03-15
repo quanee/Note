a56ed6f3350ba701a48c13385b9510dd8b881e5a
quanee <262504455@qq.com>
2017-03-15 14:20:17 +0800

    update file Python25.py

--- Python/Python/Python25.py
+++ Python/Python/Python25.py
@@ -34,3 +34,15 @@ li = [1, 2, 3, 4, 5]
 
 def pri():
     while li:
+        a = li[-1]
+        print(a)
+        time.sleep(1)
+        try:
+            li.remove(a)
+        except Exception:
+            print('----', a)
+
+
+t1 = threading.Thread(target=pri, args=())
+t1.start()
+t2 = threading.Thread(target=pri, args=())
