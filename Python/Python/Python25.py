8081d73b240b1f2b777236e6e75149bdc1362ffa
quanee <262504455@qq.com>
2017-03-15 14:20:15 +0800

    update file Python25.py

---
+++ Python/Python/Python25.py
@@ -0,0 +1,12 @@
+import queue
+
+
+'''
+q.qsize() 返回队列的大小
+q.empty() 如果队列为空，返回True,反之False
+q.full() 如果队列满了，返回True,反之False
+q.full 与 maxsize 大小对应
+q.get([block[, timeout]]) 获取队列，timeout等待时间
+q.get_nowait() 相当q.get(False)
+非阻塞 q.put(item) 写入队列，timeout等待时间
+q.put_nowait(item) 相当q.put(item, False)
