cf1d42d4eb188ad1184f555c358668f4f0d964db
quanee <262504455@qq.com>
2017-03-17 14:20:21 +0800

    update file person.py

--- Python/leaning python/oop/person.py
+++ Python/leaning python/oop/person.py
@@ -39,3 +39,11 @@ class Department:
 
     def addMember(self, person):
         self.members.append(person)
+
+    def giveRaises(self, percent):
+        for person in self.members:
+            person.giveRaise(percent)
+
+    def showAll(self):
+        for person in self.members:
+            print(person)
