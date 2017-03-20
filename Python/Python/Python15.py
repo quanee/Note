963b4374e21257508f3b20d52e09240c208a9746
quanee <262504455@qq.com>
2017-03-20 14:20:23 +0800

    update file Python15.py

--- Python/Python/Python15.py
+++ Python/Python/Python15.py
@@ -4,3 +4,11 @@
 class Foo(object):
     def __init__(self, name):
         self.name = name
+
+    def show(self):
+        return self.name
+
+
+b = 'name'
+obj = Foo('moon')
+name = obj.__dict__[b]
