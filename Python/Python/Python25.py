d775d118c926207f147af4b484d43d04da1ba026
quanee <262504455@qq.com>
2017-03-22 13:37:15 +0800

    update file Python25.py

--- Python/Python/Python25.py
+++ Python/Python/Python25.py
@@ -51,3 +51,22 @@ t2.start()
 
 import random
 
+
+class Production(threading.Thread):
+    def run(self):
+        while True:
+            r = random.randint(0, 100)
+            q.put(r)
+            print("生产出来%s号包子" % r)
+            time.sleep(1)
+
+
+class Proces(threading.Thread):
+    def run(self):
+        while True:
+            re = q.get()
+            print("吃掉%s号包子" % re)
+
+
+if __name__ == "__main__":
+    q = queue.Queue(10)
