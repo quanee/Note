697047e06f098b06ad1ff0ebeea35de2395e3cb2
quanee <262504455@qq.com>
2017-03-24 13:37:17 +0800

    update file person.py

--- Python/leaning python/oop/person.py
+++ Python/leaning python/oop/person.py
@@ -36,3 +36,22 @@ class Manager(Person):
 class Department:
     def __init__(self, *args):
         self.members = list(args)
+
+    def addMember(self, person):
+        self.members.append(person)
+
+    def giveRaises(self, percent):
+        for person in self.members:
+            person.giveRaise(percent)
+
+    def showAll(self):
+        for person in self.members:
+            print(person)
+
+
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob)
+    print(sue)
