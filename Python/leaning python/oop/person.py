25ab3ec6874c64f8a6c4ee7e6133724e726c03d2
quanee <262504455@qq.com>
2017-03-24 13:37:17 +0800

    update file person.py

--- Python/leaning python/oop/person.py
+++ Python/leaning python/oop/person.py
@@ -16,3 +16,23 @@ class Person(AttrDisplay):
 
     def lastName(self):
         return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+    # def __str__(self):
+    #     return '[Person: %s, %s]' % (self.name, self.pay)
+
+
+class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, 'mgr', pay)
+
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent + bonus)
+
+
+# 继承 复合
+class Department:
+    def __init__(self, *args):
+        self.members = list(args)
