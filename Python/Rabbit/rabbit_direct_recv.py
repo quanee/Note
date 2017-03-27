54b6d5cca02544504a48d077850dd48da6416f29
quanee <262504455@qq.com>
2017-03-27 14:20:35 +0800

    update file rabbit_direct_recv.py

--- Python/Rabbit/rabbit_direct_recv.py
+++ Python/Rabbit/rabbit_direct_recv.py
@@ -12,3 +12,7 @@ channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
 
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
+
+severities = sys.argv[1:]
+if not severities:
+    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
