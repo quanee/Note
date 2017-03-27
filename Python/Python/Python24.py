4530605fec8cafd205b709e631f87d73d20f592f
quanee <262504455@qq.com>
2017-03-27 13:37:22 +0800

    update file Python24.py

--- Python/Python/Python24.py
+++ Python/Python/Python24.py
@@ -9,3 +9,7 @@ event.wait()：如果 event.isSet()==False将阻塞线程 默认False
 event.set()： 设置event的状态值为True，所有阻塞池的线程激活进入就绪状态， 等待操作系统调度；
 event.clear()：恢复event的状态值为False。
 '''
+
+
+class Boss(threading.Thread):
+    def run(self):
