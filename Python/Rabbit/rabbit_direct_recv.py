7e5c83992c9c615b21fe018fda14d00536cd03ad
quanee <262504455@qq.com>
2017-03-27 14:20:35 +0800

    update file rabbit_direct_recv.py

--- Python/Rabbit/rabbit_direct_recv.py
+++ Python/Rabbit/rabbit_direct_recv.py
@@ -9,3 +9,6 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 channel = connection.channel()
 
 channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
