2f247c01c8234facdea0f0c5923e809c90b99507
quanee <262504455@qq.com>
2017-03-28 14:20:38 +0800

    update file conway.py

--- Python/python playground/conway/conway.py
+++ Python/python playground/conway/conway.py
@@ -7,3 +7,14 @@ import matplotlib.animation as animation
 ON = 255
 OFF = 0
 vals = [ON, OFF]
+
+
+def randomGrid(N):
+    """returns a grid of NxN random values"""
+    return np.random.choice(vals, N * N, p=[0.2, 0.8]).reshape(N, N)
+
+
+def addGlider(i, j, grid):
+    """adds a glider with top-left cell at (i, j)"""
+
+    glider = np.array([0, 0, 255],
