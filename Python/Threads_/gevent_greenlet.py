10c0d0654bd3b58d9421d3eb4e16653abef235b7
quanee <262504455@qq.com>
2017-03-31 14:20:44 +0800

    update file gevent_greenlet.py

--- Python/Threads_/gevent_greenlet.py
+++ Python/Threads_/gevent_greenlet.py
@@ -11,3 +11,11 @@ def task(method, url, req_kwargs):
     print(method, url, req_kwargs)
     response = requests.request(method=method, url=url, **req_kwargs)
     print(response.url, response.content)
+
+
+# 发送请求 (一次全部发送)
+# gevent.joinall([
+#       gevent.spawn(task, method='get', url='http://www.baidu.com/', req_kwargs={}),
+#       gevent.spawn(task, method='get', url='http://www.bing.com/', req_kwargs={}),
+#       gevent.spawn(task, method='get', url='http://www.sougou.com/', req_kwargs={}),
+#     ])
