ae90a2df7b37f5be769e2a5ff8e415ac7c68dfd3
quanee <262504455@qq.com>
2017-03-31 14:20:43 +0800

    update file gevent_greenlet.py

--- Python/Threads_/gevent_greenlet.py
+++ Python/Threads_/gevent_greenlet.py
@@ -4,3 +4,10 @@ monkey.patch_all()
 import gevent
 import requests
 '''gevent(协程)+requests(请求)'''
+
+
+
+def task(method, url, req_kwargs):
+    print(method, url, req_kwargs)
+    response = requests.request(method=method, url=url, **req_kwargs)
+    print(response.url, response.content)
