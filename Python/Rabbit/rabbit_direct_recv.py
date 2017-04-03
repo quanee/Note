36f1e94ac1f594beff0b670bc9cc8a7746e7c824
quanee <262504455@qq.com>
2017-04-03 13:37:35 +0800

    update file rabbit_direct_recv.py

--- Python/Rabbit/rabbit_direct_recv.py
+++ Python/Rabbit/rabbit_direct_recv.py
@@ -14,3 +14,5 @@ result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
 
 severities = sys.argv[1:]
+if not severities:
+    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
