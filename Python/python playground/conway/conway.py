06d2a8902248b0b98b4794f69defc3ba5555f294
quanee <262504455@qq.com>
2017-04-04 13:37:39 +0800

    update file conway.py

--- Python/python playground/conway/conway.py
+++ Python/python playground/conway/conway.py
@@ -36,3 +36,17 @@ def update(frameNum, img, grid, N):
                          grid[(i - 1) % N, j] + grid[(i + 1) % N, j] +
                          grid[(i - 1) % N, (j - 1) % N] + grid[(i - 1), (j + 1) % N] +
                          grid[(i + 1) % N, (j - 1) % N] + grid[(i + 1), (j + 1) % N]) / 255)
+
+            # apply Conway's rules
+            if grid[i, j] == ON:
+                if (total < 2) or (total > 3):
+                    newGrid[i, j] = OFF
+            else:
+                if total == 3:
+                    newGrid[i, j] = ON
+
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+
+    return img,
