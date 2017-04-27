94baa41b2240f1a0e240cd864c1ef245a093bfa1
quanee <262504455@qq.com>
2017-04-27 14:21:23 +0800

    update file Python27.py

--- Python/Python/Python27.py
+++ Python/Python/Python27.py
@@ -13,3 +13,20 @@ if __name__ == '__main__':
         p = Process(target=f, args=('moonboss',))
         p_list.append(p)
         p.start()
+
+    for i in p_list:
+        i.join()
+    print('end')
+'''
+'''
+class MyProcess(Process):
+    def __init__(self, name):
+        super(MyProcess, self).__init__()
+        self.name = name
+
+    def run(self):
+        time.sleep(1)
+        print('hello', self.name, time.ctime())
+
+
+if __name__ == '__main__':
