a98873c2bff4d599819eef9656324cb4ffb415b2
quanee <262504455@qq.com>
2017-05-01 14:21:25 +0800

    update file rabbit_topic_recv.py

--- Python/Rabbit/rabbit_topic_recv.py
+++ Python/Rabbit/rabbit_topic_recv.py
@@ -14,3 +14,10 @@ queue_name = result.method.queue
 
 binding_keys = sys.argv[1:]
 if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(exchange='topic_logs', queue=queue_name, routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
