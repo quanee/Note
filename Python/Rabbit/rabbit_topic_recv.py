afbac3e4d23a003e0b735aef1a48d6813f284d32
quanee <262504455@qq.com>
2017-05-01 14:21:24 +0800

    update file rabbit_topic_recv.py

---
+++ Python/Rabbit/rabbit_topic_recv.py
@@ -0,0 +1,8 @@
+import pika
+import sys
+
+
+credentials = pika.PlainCredentials('pangdahia', 'moonboss')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
+channel = connection.channel()
