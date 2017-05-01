b5791c46b7d0069df2689a6fae1b9bca92abbb9f
quanee <262504455@qq.com>
2017-05-01 14:21:24 +0800

    update file rabbit_topic_recv.py

--- Python/Rabbit/rabbit_topic_recv.py
+++ Python/Rabbit/rabbit_topic_recv.py
@@ -6,3 +6,11 @@ credentials = pika.PlainCredentials('pangdahia', 'moonboss')
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
 channel = connection.channel()
+
+channel.exchange_declare(exchange='topic_logs', type='topic')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = sys.argv[1:]
+if not binding_keys:
