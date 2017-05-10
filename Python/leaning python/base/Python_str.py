901f8f39f082c73f0d477664eaeb7369a6019cf3
quanee <262504455@qq.com>
2017-05-10 14:21:48 +0800

    update file Python_str.py

--- Python/leaning python/base/Python_str.py
+++ Python/leaning python/base/Python_str.py
@@ -18,3 +18,8 @@ values = {'name': 'moonboss', 'age': 40}
 print(reply % values)
 
 # vars()返回字典 包含本函数调用时存在的变量
+print(vars())
+
+# 3.x格式化方法
+print('{0}, {1} and {2}'.format('moon', 'boss', 'max'))
+print('{name}, {nicname}, {age}'.format(name='moonboss', nicname='MaxKim', age='23'))
