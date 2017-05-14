b47757034b69b3dcd9ada9451bc26e09e5bfa9ab
quanee <262504455@qq.com>
2017-05-14 14:21:57 +0800

    update file cmd_client.py

--- Python/Python/cmd_client.py
+++ Python/Python/cmd_client.py
@@ -12,3 +12,9 @@ sk.connect(address)  # 连接服务端 1
 '''
 while True:
     inp = input('>>>')
+    if inp == 'exit':
+        break
+    sk.send(bytes(inp, 'utf8'))
+    # 接受字符长度
+    result_len = int(str(sk.recv(1024), 'utf8'))
+    print(result_len)
