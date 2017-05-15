905420e8fe095754885f227c546f1e32f90c175f
quanee <262504455@qq.com>
2017-05-15 14:21:59 +0800

    update file task.py

--- Python/Celery/task.py
+++ Python/Celery/task.py
@@ -7,3 +7,6 @@ from celery import Celery
     $ celery -A task worker --loglevel=info
     $ python
     >>> from task import add
+    >>> from task import run_cmd
+    >>> add.delay(x, y)
+    >>> r = run_cmd.delay('ls')
