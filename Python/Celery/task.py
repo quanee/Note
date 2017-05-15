cc4da14995884cd83febde32eaa11d29df802793
quanee <262504455@qq.com>
2017-05-15 14:22:02 +0800

    update file task.py

--- Python/Celery/task.py
+++ Python/Celery/task.py
@@ -24,3 +24,7 @@ def add(x, y):
     print('running...', x, y)
     return x + y
 
+
+@app.task
+def run_cmd(cmd):
+    cmd_obj = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
