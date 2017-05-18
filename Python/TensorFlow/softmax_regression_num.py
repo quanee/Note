17be7549fca25647e6823a88032f64caf592b8d9
quanee <262504455@qq.com>
2017-05-18 14:22:05 +0800

    update file softmax_regression_num.py

--- Python/TensorFlow/softmax_regression_num.py
+++ Python/TensorFlow/softmax_regression_num.py
@@ -28,3 +28,7 @@ x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 # y=softmax(Wx+b)
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
