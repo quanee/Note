d24b100751d0e6b979a677f8b6b8e17832d4837e
quanee <262504455@qq.com>
2017-05-19 14:22:07 +0800

    update file 自定义异步IO框架.py

--- Python/Threads_/自定义异步IO框架.py
+++ Python/Threads_/自定义异步IO框架.py
@@ -33,3 +33,40 @@ except Exception as e:
 # 2. 发送消息
 sk.send(b'GET /index HTTP/1.0\r\nHost: baidu.com\r\n\r\n')
 # sk.send(b'POST /index HTTP/1.0\r\nHost: baidu.com\r\n\r\nk1=v1?k2=v2')
+
+# 3. 等待服务端响应
+data = sk.recv(8096)  # IO阻塞
+print(data)
+
+# 关闭连接
+sk.close()
+"""
+
+
+class HttpRequest(object):
+    """docstring for HttpRequest"""
+    def __init__(self, sk, host, callback):
+        self.socket = sk
+        self.host = host
+        self.callback = callback
+
+    def fileno(self):
+        return self.socket.fileno()
+
+
+class HttpResponse(object):
+    """docstring for HttpResponse"""
+    def __init__(self, recv_data):
+        self.recv_data = recv_data
+        self.header_dict = {}
+        self.body = None
+
+        self.initialize()
+
+    def initialize(self):
+        headers, body = self.recv_data.split(b'\r\n\r\n', 1)
+        header_list = headers.split(b'\r\n')
+        for h in header_list:
+            h_str = str(h, encoding='utf-8')
+            v = h_str.split(':', 1)
+            if len(v) == 2:
