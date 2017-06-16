c90b8f118fe4e7cc810f38cc991abc3ccdeffbba
quanee <262504455@qq.com>
2017-06-16 14:22:49 +0800

    update file Python09.py

--- Python/Python/Python09.py
+++ Python/Python/Python09.py
@@ -5,3 +5,10 @@
 import xml.etree.ElementTree as ET
 
 tree = ET.parse("xmltest.xml")
+root = tree.getroot()
+print(root.tag)
+
+# 遍历xml文档
+for child in root:
+    print(child.tag, child.attrib)
+    for i in child:
