6cbac9d0868be009b27c7d8ca596e3a4f616201a
quanee <262504455@qq.com>
2017-06-16 14:22:51 +0800

    update file Python09.py

--- Python/Python/Python09.py
+++ Python/Python/Python09.py
@@ -26,3 +26,10 @@ for node in root.iter('year'):
     new_year = int(node.text) + 1
     node.text = str(new_year)
     node.set("updated", "yes")
+
+tree.write("xmltest.xml")
+
+
+# 删除node
+for country in root.findall('country'):
+    rank = int(country.find('rank').text)
