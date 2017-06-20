3fa9afce58a77d58620b74eb6f9dce1c2641f011
quanee <262504455@qq.com>
2017-06-20 14:22:54 +0800

    update file Python05.py

--- Python/Python/Python05.py
+++ Python/Python/Python05.py
@@ -124,3 +124,24 @@ m.update(b'''It's been a long time since last time we ...''')
 print(m.digest())  # 2进制格式hash
 print(len(m.hexdigest()))  # 16进制格式hash
 '''
+def digest(self, *args, **kwargs): # real signature unknown
+    """ Return the digest value as a string of binary data. """
+    pass
+
+def hexdigest(self, *args, **kwargs): # real signature unknown
+    """ Return the digest value as a string of hexadecimal digits. """
+    pass
+
+'''
+
+# ######## md5 ########
+
+hash = hashlib.md5()
+hash.update('admin')
+print(hash.hexdigest())
+
+# ######## sha1 ########
+
+hash = hashlib.sha1()
+hash.update('admin')
+print(hash.hexdigest())
