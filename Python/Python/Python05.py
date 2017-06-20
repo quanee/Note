2543e88a80dabf85363061b9ae9c79a0521138a9
quanee <262504455@qq.com>
2017-06-20 14:22:56 +0800

    update file Python05.py

--- Python/Python/Python05.py
+++ Python/Python/Python05.py
@@ -208,3 +208,24 @@ logging.warning('is when this event was logged.')
 # %(message)s 	用户输出的消息
 
 # create logger
+logger = logging.getLogger('TEST-LOG')
+logger.setLevel(logging.DEBUG)
+
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create file handler and set level to warning
+fh = logging.FileHandler('access.log')
+fh.setLevel(logging.WARNING)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch and fh
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+# add ch and fh to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
