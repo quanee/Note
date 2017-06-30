8bbfc8033ef0bcc478685bccd2fabffd3d63c856
quanee <262504455@qq.com>
2017-06-30 14:22:59 +0800

    update file Python33.py

---
+++ Python/Python/Python33.py
@@ -0,0 +1,9 @@
+'''
+MySQL-Python
+    mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
+
+pymysql
+    mysql+pymysql://<username>:<password>@<host>/<dbname>[?<options>]
+
+MySQL-Connector
+    mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>
