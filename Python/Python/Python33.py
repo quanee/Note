06b9d68b8bbeb9d2d20fdedc272ee505e20c4901
quanee <262504455@qq.com>
2017-06-30 14:23:00 +0800

    update file Python33.py

--- Python/Python/Python33.py
+++ Python/Python/Python33.py
@@ -15,3 +15,13 @@ cx_Oracle
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from sqlalchemy import create_engine
+
+
+engine = create_engine("mysql+pymysql://root:123@127.0.0.1:3306/t1", max_overflow=5)
+
+# 执行SQL
+# cur = engine.execute(
+#     "INSERT INTO hosts (host, color_id) VALUES ('1.1.1.22', 3)"
+# )
+
+# 新插入行自增ID
