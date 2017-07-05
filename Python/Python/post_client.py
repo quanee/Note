571c96770d329552a0468a5668b225690e3b14e0
quanee <262504455@qq.com>
2017-07-05 14:23:13 +0800

    update file post_client.py

--- Python/Python/post_client.py
+++ Python/Python/post_client.py
@@ -13,3 +13,8 @@ sk.connect(address)  # 连接服务端 1
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 while True:
+    inp = input('>>>').strip()  # post|18000.jpg
+
+    cmd, path = inp.split('|')
+
+    os.path.join(BASE_DIR, path)
