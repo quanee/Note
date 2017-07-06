60ca19884fe79891f6bcf09b076150fca51c4da2
quanee <262504455@qq.com>
2017-07-06 14:23:16 +0800

    update file Python03.py

--- Python/Python/Python03.py
+++ Python/Python/Python03.py
@@ -30,3 +30,19 @@ print(b)
 t = ['123', 8]
 
 a, b = t
+print(a[0], b)
+
+
+# 生成器 需要时生成 不能随机访问
+# 生成器都是迭代器 迭代器不一定是生成器
+# 生成器在创建时已经决定能生成多少个值
+# 生成器方法
+s = (x * 2 for x in range(10))
+print(s.__next__())  # 特殊方法
+print(next(s))  # Python3 内置方法 Python2 s.next()
+
+
+def fib(max):
+    n, before, after = 0, 0, 1
+    while n < max:
+        print(before)
