cd45516825db8e3329b30d6eec7e8d0814164cf0
quanee <262504455@qq.com>
2017-07-06 14:23:15 +0800

    update file Python03.py

---
+++ Python/Python/Python03.py
@@ -0,0 +1,16 @@
+from collections import Iterator
+
+# 迭代器 生成器
+"""
+可迭代(Iterable)
+    list, tuple, dict, set, str
+    generator, yield function
+可迭代对象(isinstance(x, Iterator))
+    generator
+"""
+
+# 生成器创建方式
+# (x for x in range(123)) 列表生成式
+# yield
+
+# 列表生成式
