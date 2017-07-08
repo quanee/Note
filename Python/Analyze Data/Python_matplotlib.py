eb2044e68148d46ea4fa4a12f4803c406ca2c85a
quanee <262504455@qq.com>
2017-07-08 14:23:18 +0800

    update file Python_matplotlib.py

--- Python/Analyze Data/Python_matplotlib.py
+++ Python/Analyze Data/Python_matplotlib.py
@@ -86,3 +86,90 @@ clf_cla_close(plt)
 u = np.linspace(-3, 3, 30)
 x, y = np.meshgrid(u, u)
 r = 0.6
+mux = 0
+muy = 0
+sx = 1
+sy = 1
+z = (1 / (2 * 3.1415926535 * sx * sy * np.sqrt(1 - r * r))) * np.exp((-1 / (2 * (1 - r * r))) * (((x - mux) ** 2) / (sx ** 2) - 2 * r * (x - mux) * (y - muy) / (sx * sy) + ((y - muy) ** 2) / (sx ** 2)))
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.Greys)
+# plt.show()
+clf_cla_close(plt)
+
+
+# 绘制随机折线图
+plt.plot(np.random.randn(100), linewidth=2.0)
+# plt.show()
+clf_cla_close(plt)
+
+
+# jd_stock
+plt.plot(jd_stock['opening_price'])
+# plt.show()
+clf_cla_close(plt)
+
+
+# 图像基本设置
+# 创建图例
+plt.plot(jd_stock['opening_price'], label='Opening Price')
+plt.plot(jd_stock['closing_price'], label='Closing Price')
+# fromeon是否有框
+plt.legend(loc='lower right', frameon=False)
+'''
+图例标位置参数
+    upper right: 1
+    upper left: 2
+    lower left: 3
+    lower right: 4
+    right: 5
+    center left: 6
+    center right: 7
+    lower center: 8
+    uppercenter: 9
+    center: 10
+'''
+# plt.show()
+clf_cla_close(plt)
+
+# 刻度设置
+X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+C, S = np.cos(X), np.sin(X)
+plt.plot(X, C, '--')
+plt.plot(X, S)
+plt.xticks([-np.pi, -np.pi / 2, 0, np.pi / 2, np.pi], [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
+plt.yticks([-1, 0, +1], [r'$-1$', r'$0$', r'$+1$'])
+
+# 图像注解
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data', 0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data', 0))
+
+t = 2 * np.pi / 3
+plt.plot([t, t], [0, np.cos(t)], color='blue', linewidth=2.5, linestyle='--')
+plt.scatter([t, ], [np.cos(t), ], 50, color='blue')
+
+plt.annotate(r'$\sin(\frac{2\pi}{3})=\frac{\sqrt{3}}{2}$', xy=(t, np.sin(t)), xycoords='data', xytext=(+10, +30), textcoords='offset points', fontsize=16, arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=.2'))
+
+plt.plot([t, t], [0, np.sin(t)], color='red', linewidth=2.5, linestyle='--')
+plt.scatter([t, ], [np.sin(t), ], 50, color='red')
+
+plt.annotate(r'$\cos(\frac{2\pi}{3})=-\frac{1}{2}$', xy=(t, np.cos(t)), xycoords='data', xytext=(-90, -50), textcoords='offset points', fontsize=16, arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=.2'))
+clf_cla_close(plt)
+
+
+# 图像大小
+plt.plot(jd_stock['opening_price'], label='Opening Price')
+plt.plot(jd_stock['closing_price'], label='Closing Price')
+plt.legend(loc='upper left', frameon=True)
+plt.xticks([xticks for xticks in range(71)], list(jd_stock['time']), rotation=40)
+# 设置图像对象名为fig
+fig = plt.gcf()
+fig.set_size_inches(18.5, 10.5)
+# 显示网格
+plt.grid()
+clf_cla_close(plt)
