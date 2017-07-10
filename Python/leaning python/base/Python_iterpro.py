64394ff42a4206fb602103e6a8e63d3b1b80092e
quanee <262504455@qq.com>
2017-07-10 14:23:24 +0800

    update file Python_iterpro.py

--- Python/leaning python/base/Python_iterpro.py
+++ Python/leaning python/base/Python_iterpro.py
@@ -118,3 +118,27 @@ def mymap(func, *seqs):
         yield func(*args)
 
 # (...)
+def mymap(func, *seqs):
+    return (func(*args) for args in zip(*seqs))
+
+
+# 生成器模拟zip
+def myzip(*seqs):
+    seqs = [list(S) for S in seqs]
+    res = []
+    while all(seqs):
+        res.append(tuple(S.pop(0) for S in seqs))
+    return res
+
+
+def mymapPad(*seqs, pad=None):
+    seqs = [list(S) for S in seqs]
+    res = []
+    while any(seqs):
+        res.append(tuple((S.pop(0) if S else pad) for S in seqs))
+    return res
+
+
+S1, S2 = 'abc', 'xyz123'
+print(myzip(S1, S2))  # [('a', 'x'), ('b', 'y'), ('c', 'z')]
+print(mymapPad(S1, S2))  # [('a', 'x'), ('b', 'y'), ('c', 'z'), (None, '1'), (None, '2'), (None, '3')]
