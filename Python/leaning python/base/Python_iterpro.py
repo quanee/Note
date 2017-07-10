426a177b0ab7bb7a32a44987b49b4c7b1ae0d98d
quanee <262504455@qq.com>
2017-07-10 14:23:22 +0800

    update file Python_iterpro.py

--- Python/leaning python/base/Python_iterpro.py
+++ Python/leaning python/base/Python_iterpro.py
@@ -22,3 +22,27 @@ M = [[1, 2, 3],
 N = [[2, 2, 2],
      [3, 3, 3],
      [4, 4, 4]]
+
+
+print(M[1])
+print(M[1][2])
+
+print([row[1] for row in M])
+print([M[row][1] for row in (0, 1, 2)])
+print([M[i][i] for i in range(len(M))])
+
+print([M[row][col] * N[row][col] for row in range(3) for col in range(3)])
+print([[M[row][col] * N[row][col] for col in range(3)] for row in range(3)])
+
+
+# 生成器
+def gen():
+    for i in range(10):
+        X = yield i
+        print(X)
+
+
+G = gen()
+print(next(G))  # send(None)
+print(G.send(77))
+print(G.send(88))
