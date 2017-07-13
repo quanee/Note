ef1fae0c54aa66a837d479435c2aa742dc32a9ee
quanee <262504455@qq.com>
2017-07-13 14:23:34 +0800

    update file HDF5_1.py

--- Python/HDF5/HDF5_1.py
+++ Python/HDF5/HDF5_1.py
@@ -22,3 +22,9 @@ import h5py
 f = h5py.File("weather.hdf5", "w")
 f["/15/temperature"] = temperature
 f["/15/temperature"].attrs["dt"] = 10.0
+f["/15/temperature"].attrs["start_time"] = 1475204299
+f["/15/wind"] = wind
+f["/15/wind"].attrs["dt"] = 5.0
+
+dataset = f["/15/temperature"]
+for key, value in dataset.attrs.items():
