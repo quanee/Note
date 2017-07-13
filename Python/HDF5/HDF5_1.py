b80af97b0bde7fbb61c46355a86c9458ad0d8b46
quanee <262504455@qq.com>
2017-07-13 14:23:32 +0800

    update file HDF5_1.py

--- Python/HDF5/HDF5_1.py
+++ Python/HDF5/HDF5_1.py
@@ -4,3 +4,8 @@ temperature = np.random.random(1024)
 print(temperature)
 
 dt = 10
+start_time = 1475204299
+station = 15
+np.savez("weather.npz", data=temperature, start_time=start_time, station=station)
+
+out = np.load("weather.npz")
