e4b62f6ef1bba6090cf9e1250ee5af7e7de2f7d7
quanee <262504455@qq.com>
2017-07-15 14:23:39 +0800

    update file HDF5_6.py

--- Python/HDF5/HDF5_6.py
+++ Python/HDF5/HDF5_6.py
@@ -2,3 +2,9 @@ import h5py
 import numpy as np
 
 
+
+f = h5py.File('attrsdemo.hdf5', 'w')
+dset = f.create_dataset('dataset', (100, ))
+print(dset.attrs)
+dset.attrs['title'] = 'Dataset from third round of experiments'
+dset.attrs['sample_rate'] = 100e6
