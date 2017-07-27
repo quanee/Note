aa6b15945c1358e377e0c69d3d14df79623ea6ed
quanee <262504455@qq.com>
2017-07-27 14:26:00 +0800

    update file rabbit_fanout_send.py

--- Python/Rabbit/rabbit_fanout_send.py
+++ Python/Rabbit/rabbit_fanout_send.py
@@ -6,3 +6,5 @@ credentials = pika.PlainCredentials('pangdahia', 'moonboss')
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
 channel = connection.channel()
+
+# 声明名为logs的exchange类型为fanout
