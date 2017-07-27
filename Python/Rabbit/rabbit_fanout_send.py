49b6b30e683f141f9a576313333f9a7a706f770b
quanee <262504455@qq.com>
2017-07-27 14:25:59 +0800

    update file rabbit_fanout_send.py

--- Python/Rabbit/rabbit_fanout_send.py
+++ Python/Rabbit/rabbit_fanout_send.py
@@ -3,3 +3,6 @@ import sys
 """广播"""
 
 credentials = pika.PlainCredentials('pangdahia', 'moonboss')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
+channel = connection.channel()
