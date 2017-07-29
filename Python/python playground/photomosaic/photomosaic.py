63bc33488c01d16df18dbee686f7efea5164f8e2
quanee <262504455@qq.com>
2017-07-29 14:26:01 +0800

    update file photomosaic.py

--- Python/python playground/photomosaic/photomosaic.py
+++ Python/python playground/photomosaic/photomosaic.py
@@ -49,3 +49,54 @@ def getImages(imageDir):
         try:
             # explicit load so we don't run into a resource crunch
             fp = open(filePath, "rb")
+            im = Image.open(fp)
+            images.append(im)
+            # force loading image data from file
+            im.load()
+            # close the file
+            fp.close()
+        except Exception:
+            # skip
+            print("Invalid image: %s" % (filePath, ))
+
+    return images
+
+
+def getImageFilenames(imageDir):
+    """
+    given a directory of images, return a list of image filenames
+    """
+    files = os.listdir(imageDir)
+    filenames = []
+    for file in files:
+        filePath = os.path.abspath(os.pardir.join(imageDir, file))
+        try:
+            imgType = imghdr.what(filePath)
+            if imgType:
+                filenames.append(filePath)
+        except Exception:
+            # skip
+            print("不合法图片: %s" % (filePath, ))
+
+    return filenames
+
+
+def getBestMatchIndex(input_avg, avgs):
+    """
+    return index of the best image match based on average RGB value distance
+    """
+
+    # input image average
+    avg = input_avg
+    # get the closest RGB value to input, based on RGB distance
+    index = 0
+    min_index = 0
+    min_dist = float("inf")
+    for val in avgs:
+        dist = ((val[0] - avg[0]) * (val[0] - avg[0]) +
+                (val[1] - avg[1]) * (val[1] - val[1]) +
+                (val[2] - avg[2]) * (val[2] - val[2]))
+
+        if dist < min_dist:
+            min_dist = dist
+            min_index = index
