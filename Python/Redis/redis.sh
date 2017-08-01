35a77bbabfd6c94142b090726da47cb204ce0b5c
quanee <262504455@qq.com>
2017-08-01 14:26:11 +0800

    update file redis.sh

--- Python/Redis/redis.sh
+++ Python/Redis/redis.sh
@@ -2,3 +2,7 @@
 # chkconfig:   2345 90 10
 # description:  Redis is a persistent key-value database
 # redis    Startup script for redis processes
+# processname: redis
+redis_path="/usr/local/bin/redis-server"
+redis_conf="/etc/redis.conf"
+redis_pid="/var/run/redis.pid"
