1cae286b458f50ab86c97db3e0f10102d8c12298
quanee <262504455@qq.com>
2017-08-01 14:26:11 +0800

    update file redis.sh

--- Python/Redis/redis.sh
+++ Python/Redis/redis.sh
@@ -6,3 +6,7 @@
 redis_path="/usr/local/bin/redis-server"
 redis_conf="/etc/redis.conf"
 redis_pid="/var/run/redis.pid"
+# Source function library.
+. /etc/rc.d/init.d/functions
+[ -x $redis_path ] || exit 0
+RETVAL=0
