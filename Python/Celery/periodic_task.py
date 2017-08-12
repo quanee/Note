d71d3ea560a20273203d4ea87b8e7e0031692f48
quanee <262504455@qq.com>
2017-08-12 14:26:29 +0800

    update file periodic_task.py

--- Python/Celery/periodic_task.py
+++ Python/Celery/periodic_task.py
@@ -1,3 +1,6 @@
 from celery import Celery
 from celery.schedules import crontab
 """通过调用函数添加定时任务定时任务"""
+
+ 
+app = Celery('task', broker='redis://:password@//localhost:port', backend='redis://:password@//localhost:port')
