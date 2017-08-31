acf11687d8d98f96813b73d95fe4c8439132d084
quanee <262504455@qq.com>
2017-08-31 14:27:08 +0800

    update file ascii.py

--- Python/python playground/ascii/ascii.py
+++ Python/python playground/ascii/ascii.py
@@ -43,3 +43,12 @@ def covertImageToAscii(fileName, cols, scale, moreLevels):
     w = W / cols
     # compute tile height based on the aspect ratio and scale of the font
     h = w / scale
+    # compute number of rows to use in the final grid
+    rows = int(H / h)
+
+    print("col: %d, rows: %d" % (cols, rows))
+    print("tile dims: %d x %d" % (w, h))
+
+    # chech if image size is too small
+    if cols > W or rows > H:
+        print("Image too small for specified cols!")
