6e4729379d11384cda68181989b2d9e019de5d30
quanee <262504455@qq.com>
2017-09-09 14:27:26 +0800

    update file multiprocess.py

--- Python/Python/multiprocess.py
+++ Python/Python/multiprocess.py
@@ -38,3 +38,13 @@ def long_time_task(name):
     end = time.time()
     print("Task %s run %0.f seconds." % (name, (end - start)))
     if __name__ == "__main__":
+        print("Parent process %s." % os.getpid())
+        p = Pool(4)
+        for i in range(5):
+            p.apply_async(long_time_task, args=(i,))
+            print("Waiting for all subprocess done...")
+            p.close()
+            p.join()
+            print("All subprocess done.")
+
+'''
