796f9b8907a52ac843dff4badc11eb178914ee99
quanee <262504455@qq.com>
2017-09-09 14:27:26 +0800

    update file multiprocess.py

--- Python/Python/multiprocess.py
+++ Python/Python/multiprocess.py
@@ -48,3 +48,13 @@ def long_time_task(name):
             print("All subprocess done.")
 
 '''
+对Pool对象调用join()方法会等待所有子进程执行完毕，调用join()之前必须先调用close()，
+调用close()之后就不能继续添加新的Process了。
+
+注意输出的结果，task0，1，2，3是立刻执行的，而task4要等待前面某个task完成后才执行，
+这是因为Pool的默认大小设置成了4(p = Pool(4))，代表着最多同时执行4个进程。
+这是Pool有意设计的限制，并不是操作系统的限制。如果改成:
+p = Pool(5)
+就可以跑5个进程了。
+由于Pool的默认大小是CPU的核数，如果你拥有8核CPU，提交至少9个子进程才能看到上面的等待效果。
+子进程
