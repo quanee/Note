12e7a550043d1af546a2bd1a1925d526390e11c8
quanee <262504455@qq.com>
2017-09-09 14:27:25 +0800

    update file multiprocess.py

--- Python/Python/multiprocess.py
+++ Python/Python/multiprocess.py
@@ -8,3 +8,12 @@ import random
 
 # Only works on Unix/Linux/Mac:
 print('Process (%s) start...' % os.getpid())
+pid = os.fork()
+if pid == 0:
+    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+else:
+    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+
+# multiprocessing模块调用
+""" 子进程要执行的代码"""
+
