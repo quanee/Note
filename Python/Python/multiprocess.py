71ac9aba9e8cc691d7215206dcd1749a49591ae7
quanee <262504455@qq.com>
2017-09-09 14:27:26 +0800

    update file multiprocess.py

--- Python/Python/multiprocess.py
+++ Python/Python/multiprocess.py
@@ -27,3 +27,14 @@ def run_proc(name):
         p.start()
         p.join()
         print('Child process end.')
+
+
+# 进程池 Pool
+# 如果要启动大量的子进程，用进程池的方式批量创建子进程：
+def long_time_task(name):
+    print("Run task %s (%s)..." % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print("Task %s run %0.f seconds." % (name, (end - start)))
+    if __name__ == "__main__":
