b9539ff8b1b3b86de75db0c79ac34ee96c9fd38d
quanee <262504455@qq.com>
2017-09-27 14:27:41 +0800

    update file rabbit_direct_send.py

--- Python/Rabbit/rabbit_direct_send.py
+++ Python/Rabbit/rabbit_direct_send.py
@@ -10,3 +10,6 @@ channel = connection.channel()
 
 channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
 # 严重程度(权重)
+severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
+message = ' '.join(sys.argv[2:]) or 'Hello World!'
+channel.basic_publish(exchange='direct_logs', routing_key=severity, body=message)
