da9402d828abfe1f289ebe76ea6c8f5867af1c53
quanee <262504455@qq.com>
2017-09-27 14:27:40 +0800

    update file rabbit_direct_send.py

--- Python/Rabbit/rabbit_direct_send.py
+++ Python/Rabbit/rabbit_direct_send.py
@@ -7,3 +7,6 @@ credentials = pika.PlainCredentials('pangdahia', 'moonboss')
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
 channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+# 严重程度(权重)
