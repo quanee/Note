e2e822c31362ee62814501c1c118077731331240
quanee <262504455@qq.com>
2017-09-27 14:27:40 +0800

    update file rabbit_direct_send.py

--- Python/Rabbit/rabbit_direct_send.py
+++ Python/Rabbit/rabbit_direct_send.py
@@ -4,3 +4,6 @@ import sys
 
 
 credentials = pika.PlainCredentials('pangdahia', 'moonboss')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
+channel = connection.channel()
