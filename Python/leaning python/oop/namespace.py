28edfa6406718366993d85121a38fc98e8107662
quanee <262504455@qq.com>
2017-10-02 14:27:49 +0800

    update file namespace.py

--- Python/leaning python/oop/namespace.py
+++ Python/leaning python/oop/namespace.py
@@ -24,3 +24,5 @@ if __name__ == '__main__':
     # {'data2': 'boss', 'data1': 'moon'}
     print(X.__dict__.keys())
     # dict_keys(['data2', 'data1'])
+    print(dir(X))
+    # ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'data1', 'data2', 'hello', 'hola']
