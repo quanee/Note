4279da8561c52064e686689c71fddbe8255532e5
quanee <262504455@qq.com>
2017-10-08 14:27:57 +0800

    update file ks.py

--- Python/python playground/karplus/ks.py
+++ Python/python playground/karplus/ks.py
@@ -10,3 +10,15 @@ from collections import deque
 from matplotlib import pyplot as plt
 
 # show plot of algorithm in action?
+gShowPlot = False
+
+# notes of a Pentatonic Minor scale
+# piano C4-E(b)-F-G-B(b)-C5
+pmNotes = {'C4': 262, 'Eb': 311, 'F': 349, 'G': 391, 'Bb': 466}
+
+# write out WAV file
+
+
+def writeWAVE(fname, data):
+    # open file
+    file = wave.open(fname, 'wb')
