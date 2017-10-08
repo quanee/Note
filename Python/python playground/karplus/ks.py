0c425e9048f1bb20bd62ed36850116cbdc10e87d
quanee <262504455@qq.com>
2017-10-08 14:27:57 +0800

    update file ks.py

--- Python/python playground/karplus/ks.py
+++ Python/python playground/karplus/ks.py
@@ -22,3 +22,15 @@ pmNotes = {'C4': 262, 'Eb': 311, 'F': 349, 'G': 391, 'Bb': 466}
 def writeWAVE(fname, data):
     # open file
     file = wave.open(fname, 'wb')
+    # WAV file parameters
+    nChannels = 1
+    sampleWidth = 2
+    frameRate = 44100
+    nFrames = 44100
+    # set parameters
+    file.setparams((nChannels, sampleWidth, frameRate, nFrames, 'NONE', 'noncompressed'))
+    file.writeframes(data)
+    file.close()
+
+
+# generate note of given frequency
