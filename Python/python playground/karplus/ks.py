808f96febf987391f86db8b11d855020f9e93333
quanee <262504455@qq.com>
2017-10-08 14:27:56 +0800

    update file ks.py

---
+++ Python/python playground/karplus/ks.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import time
+import random
+import wave
+import argparse
+import pygame
+import numpy as np
+from collections import deque
+from matplotlib import pyplot as plt
+
+# show plot of algorithm in action?
