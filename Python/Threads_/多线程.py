e6ba88800aa927472ae0d1b976de17f586058d43
quanee <262504455@qq.com>
2017-11-05 14:28:53 +0800

    update file 多线程.py

--- Python/Threads_/多线程.py
+++ Python/Threads_/多线程.py
@@ -7,3 +7,9 @@ def task(url):
     response = requests.get(url)
     return response
     # time.sleep(1)
+
+
+def done(future, *args, **kwargs):
+    response = future.result()
+    # print(response.content)
+    print(response.status_code)
