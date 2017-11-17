cd47dac12721ae81a37f9892a055c22c63e116ad
quanee <262504455@qq.com>
2017-11-17 14:29:07 +0800

    update file Python18.py

--- Python/Python/Python18.py
+++ Python/Python/Python18.py
@@ -10,3 +10,5 @@ header = {
           'Accept': 'application/json, text/javascript, */*; q=0.01',
           'Accept-Encoding': 'gzip, deflate, br',
           'Accept-Language': 'zh-CN,zh;q=0.9',
+          'Connection': 'keep-alive',
+          'Content-Length': '25',
