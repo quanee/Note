dbafe5fbe95c884f6e66848ccd133acf5cfd389b
quanee <262504455@qq.com>
2017-11-17 14:29:05 +0800

    update file Python18.py

--- Python/Python/Python18.py
+++ Python/Python/Python18.py
@@ -1,3 +1,6 @@
 import requests
 
 url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E6%88%90%E9%83%BD&needAddtionalResult=false&isSchoolJob=0'
+
+data = {'first': 'false',
+        'pn': 4,
