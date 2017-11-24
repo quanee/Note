acf567c37262a94b93ba3a8d538f7d1f0d76fcea
quanee <262504455@qq.com>
2017-11-24 14:29:14 +0800

    update file write_excel_to_mysql.py

--- Python/Python/write_excel_to_mysql.py
+++ Python/Python/write_excel_to_mysql.py
@@ -11,3 +11,15 @@ table = data.sheets()[0]  # 第一个sheets
 datalist = []
 
 for i in range(1, table.nrows):  # 总行数
+
+    datalist.append(tuple(table.row_values(i)))
+
+conn = sqlite3.connect('d:\\database\\country.db')  # 数据库文件的路径
+
+cursor = conn.cursor()
+
+cursor.execute("create table country(province varchar(20),num int,town varchar(30),town varchar(30))")  # #创建表
+
+sql = 'insert into country(province,num,city,town) values(?,?,?,?)'
+
+cursor.executemany(sql, datalist)  # 插入数据
