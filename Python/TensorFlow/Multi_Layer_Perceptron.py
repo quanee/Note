1198ff0ede9518b9dd120dc0ed250747a431db68
quanee <262504455@qq.com>
2017-12-01 14:29:36 +0800

    update file Multi_Layer_Perceptron.py

--- Python/TensorFlow/Multi_Layer_Perceptron.py
+++ Python/TensorFlow/Multi_Layer_Perceptron.py
@@ -13,3 +13,6 @@ W1 = tf.Variable(tf.truncated_normal([in_units, h1_units], stddev=0.1))
 b1 = tf.Variable(tf.zeros([h1_units]))
 W2 = tf.Variable(tf.zeros([h1_units, 10]))
 b2 = tf.Variable(tf.zeros([10]))
+
+x = tf.placeholder(tf.float32, [None, in_units])
+keep_prob = tf.placeholder(tf.float32)
