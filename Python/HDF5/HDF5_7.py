52b16dc11cbe6aab26d99b0a6793ab0f44242627
quanee <262504455@qq.com>
2017-12-03 14:29:40 +0800

    update file HDF5_7.py

--- Python/HDF5/HDF5_7.py
+++ Python/HDF5/HDF5_7.py
@@ -2,3 +2,7 @@ import h5py
 import numpy as np
 
 # 定长字符串
+f = h5py.File('typesdemo.hdf5')
+dt = np.dtype('S10')
+dset = f.create_dataset('fixed_string', (100, ), dtype=dt)
+dset[0] = 'Hello'.encode()
