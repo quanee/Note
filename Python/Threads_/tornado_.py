993ab2047b89a6ae00ab8f4b45deb3791a37d2f5
quanee <262504455@qq.com>
2017-12-06 14:29:48 +0800

    update file tornado_.py

--- Python/Threads_/tornado_.py
+++ Python/Threads_/tornado_.py
@@ -7,3 +7,12 @@ COUNT = 0
 def handle_response(response):
     """处理返回值内容(（)需要维护计数器，来停止IO循环),调用 ioloop.IOLoop.current().stop()"""
     global COUNT
+    COUNT -= 1
+    if response.error:
+        print("Error:", response.error)
+    else:
+        # print(response.body)
+        print('response.url')
+
+    if COUNT == 0:
+        ioloop.IOLoop.current().stop()
