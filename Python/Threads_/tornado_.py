a019c50db7129bef35b4f1a7dad0ab137a5043b0
quanee <262504455@qq.com>
2017-12-06 14:29:48 +0800

    update file tornado_.py

---
+++ Python/Threads_/tornado_.py
@@ -0,0 +1,9 @@
+from tornado.httpclient import AsyncHTTPClient
+from tornado.httpclient import HTTPRequest
+from tornado import ioloop
+
+
+COUNT = 0
+def handle_response(response):
+    """处理返回值内容(（)需要维护计数器，来停止IO循环),调用 ioloop.IOLoop.current().stop()"""
+    global COUNT
