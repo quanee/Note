ab4bbfb3a7b0bbb9631183b739d6d382ce150215
quanee <262504455@qq.com>
2017-12-08 14:29:58 +0800

    update file Python_stats.py

--- Python/Analyze Data/Python_stats.py
+++ Python/Analyze Data/Python_stats.py
@@ -12,3 +12,9 @@ pop = np.array([2, 4, 6, 8])
 print(pop.var(ddof=0))
 # 总体均值
 print(pop.mean())
+
+miu = np.array([2, 3, 4, 5, 3, 4, 5, 6, 4, 5, 6, 7, 5, 6, 8])
+print(miu.var(ddof=0))
+print(miu.mean())
+
+
