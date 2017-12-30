29e4a0479a444fa6ba9dd10f4b2aeff0aeff9189
quanee <262504455@qq.com>
2017-12-30 14:30:30 +0800

    update file makedb.py

--- Python/leaning python/oop/makedb.py
+++ Python/leaning python/oop/makedb.py
@@ -3,3 +3,8 @@ from person import Person, Manager
 bob = Person('Bob Smith')
 sue = Person('Sue Jones', job='dev', pay=100000)
 tom = Manager('Tom Jones', 50000)
+
+import shelve
+
+
+db = shelve.open('persondb')
