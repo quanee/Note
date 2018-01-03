688e1a0b932f5e4f2ed06874d59dd23b5e301d51
quanee <262504455@qq.com>
2018-01-03 14:30:42 +0800

    update file grequests_.py

--- Python/Threads_/grequests_.py
+++ Python/Threads_/grequests_.py
@@ -7,3 +7,4 @@ gevent+requests的封装
 request_list = [
     grequests.get('http://www.baidu.com/', timeout=0.1),
     grequests.get('http://www.bing.com/'),
+    grequests.get('http://www.github.com/'),
