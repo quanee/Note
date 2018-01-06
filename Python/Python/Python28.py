cd15101ec484d89b130cac4ece371d3445ad5f26
quanee <262504455@qq.com>
2018-01-06 14:30:47 +0800

    update file Python28.py

--- Python/Python/Python28.py
+++ Python/Python/Python28.py
@@ -5,3 +5,10 @@ from multiprocessing import Process, Queue, Pipe
 def f(q, n):
     q.put([42, n, 'hello'])
     print('sub id q', id(q))
+
+
+if __name__ == '__main__':
+    q = Queue()
+    print('main id q', id(q))
+    p_list = []
+    for i in range(3):
