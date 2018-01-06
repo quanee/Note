f37761e19c4891deaa0572351ad29f37eac32ad7
quanee <262504455@qq.com>
2018-01-06 14:30:49 +0800

    update file Python28.py

--- Python/Python/Python28.py
+++ Python/Python/Python28.py
@@ -33,3 +33,10 @@ def f(conn):
 
 if __name__ == '__main__':
     parent_conn, child_conn = Pipe()
+    p = Process(target=f, args=(child_conn,))
+    p.start()
+    print(parent_conn.recv())   # prints "[42, None, 'hello']"
+    print(parent_conn.recv())
+    parent_conn.send('hello')
+    p.join()
+'''
