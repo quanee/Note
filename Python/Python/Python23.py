332d7075f283ed4e0f2ee0283bda35d8501b69c8
quanee <262504455@qq.com>
2018-01-09 14:31:00 +0800

    update file Python23.py

--- Python/Python/Python23.py
+++ Python/Python/Python23.py
@@ -9,3 +9,14 @@ class Producer(threading.Thread):
         global L
         while True:
             val = randint(0, 10)
+            print('生产者', self.name, ":Append" + str(val), L)
+            if lock_con.acquire():
+                L.append(val)
+                lock_con.notify()
+                lock_con.release()
+            time.sleep(1)
+
+
+class Consumer(threading.Thread):
+    def run(self):
+        global L
