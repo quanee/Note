1d03978196c5aa8b287ff95bc9a06e4ed892083a
quanee <262504455@qq.com>
2018-01-10 14:31:03 +0800

    update file HDF5_3.py

--- Python/HDF5/HDF5_3.py
+++ Python/HDF5/HDF5_3.py
@@ -23,3 +23,27 @@ print(dset[...])
 
 # 创建空数据集
 dset = f.create_dataset("test1", (10, 10))
+print(dset)
+dset = f.create_dataset("test2", (10, 10), dtype=np.complex)
+print(dset)
+
+# 创建一个一维数据集能够装下4GB数据
+dset = f.create_dataset("big dataset", (1024**3, ), dtype=np.float32)
+dset[0:1024] = np.arange(1024)
+f.flush()
+f.close()
+
+bigdata = np.ones((100, 1000))
+print(bigdata.dtype)
+print(bigdata.shape)
+
+with h5py.File('big1.hdf5', 'w') as f1:
+    f1['big'] = bigdata
+
+with h5py.File('big2.hdf5', 'w') as f2:
+    f2.create_dataset('big', data=bigdata, dtype=np.float32)
+
+f1 = h5py.File('big1.hdf5')
+f2 = h5py.File('big2.hdf5')
+print(f1['big'].dtype)
+print(f2['big'].dtype)
