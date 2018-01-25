a86fe946af7ed1f9cce767541238b589f2e0cb1f
quanee <262504455@qq.com>
2018-01-25 14:31:29 +0800

    update file Python43.py

--- Python/Python/Python43.py
+++ Python/Python/Python43.py
@@ -9,3 +9,7 @@ try:
     socket_obj_3.connet()
 except Exception:
     ...
+
+
+while True:
+    r, w, e = select.select([socket_obj_1, socket_obj_2, socket_obj_3, ], [socket_obj_1, socket_obj_2, socket_obj_3, ], [], 0.05)
