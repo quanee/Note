7177b0a13d62ebc4a9f4be3e82120a2c309c9e42
quanee <262504455@qq.com>
2018-01-25 14:31:30 +0800

    update file Python43.py

--- Python/Python/Python43.py
+++ Python/Python/Python43.py
@@ -15,3 +15,6 @@ while True:
     r, w, e = select.select([socket_obj_1, socket_obj_2, socket_obj_3, ], [socket_obj_1, socket_obj_2, socket_obj_3, ], [], 0.05)
     r = [socket_obj_1, ]  # 2对方发数据
         xx = socket_obj_1.recv()  # 4
+
+    w = [socket_obj_1, ]  # 1与对方创建连接成功
+        socket_obj_1.send("'''GET /index HTTP/1.0\r\nHost: baidu.com\r\n\r\n'''")  # 3
