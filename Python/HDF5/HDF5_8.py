bc75e78fbb09d105c629fab1752f53f31ebbb7ff
quanee <262504455@qq.com>
2018-01-31 14:31:33 +0800

    update file HDF5_8.py

--- Python/HDF5/HDF5_8.py
+++ Python/HDF5/HDF5_8.py
@@ -6,3 +6,11 @@ import numpy as np
 # 创建
 f = h5py.File('refs_demo.hdf5', 'w')
 grp1 = f.create_group('group1')
+grp2 = f.create_group('group2')
+dset = f.create_dataset('mydata', shape=(100, ))
+print(grp1.ref)
+out = f[grp1.ref]
+print(out == grp1)
+
+# 只能定位本文件内对象
+out = grp2[grp1.ref]
