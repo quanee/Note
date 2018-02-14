05b4c3f7039dde99ca261466c3eeca1014848cbc
quanee <262504455@qq.com>
2018-02-14 14:32:03 +0800

    update file Python32.py

--- Python/Python/Python32.py
+++ Python/Python/Python32.py
@@ -10,3 +10,15 @@ conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='123', d
 cursor = conn.cursor()
 
 # 执行SQL，并返回收影响行数
+effect_row = cursor.execute("update hosts set host = '1.1.1.2'")
+
+# 执行SQL，并返回受影响行数
+# effect_row = cursor.execute("update hosts set host = '1.1.1.2' where nid > %s", (1,))
+
+# 执行SQL，并返回受影响行数
+# effect_row = cursor.executemany("insert into hosts(host,color_id)values(%s,%s)", [("1.1.1.11",1),("1.1.1.11",2)])
+
+# 获取第一行数据
+row_1 = cursor.fetchone()
+
+# 获取前n行数据
