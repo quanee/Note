efd37b3522fbe9f0ab88ba903de68e71e9466248
quanee <262504455@qq.com>
2018-02-26 14:32:25 +0800

    update file Python_decorator_private.py

--- Python/leaning python/ulm/Python_decorator_private.py
+++ Python/leaning python/ulm/Python_decorator_private.py
@@ -11,3 +11,16 @@ def trace(*args):
 def Private(*privates):
     def onDecorator(aClass):
         class onInstance:
+            def __init__(self, *args, **kargs):
+                self.wrapped = aClass(*args, **kargs)
+
+            def __getattr__(self, attr):
+                trace('get:', attr)
+
+                if attr in privates:
+                    raise TypeError('private attribute fetch: ' + attr)
+                else:
+                    return getattr(self.wrapped, attr)
+
+            def __setattr__(self, attr, value):
+                trace('set:', attr, value)
