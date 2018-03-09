0dd2476d7318a80efb4d11b37796dbbab56e4782
quanee <262504455@qq.com>
2018-03-09 14:32:47 +0800

    update file Python21.py

--- Python/Python/Python21.py
+++ Python/Python/Python21.py
@@ -6,3 +6,7 @@ import time
 class myThread(threading.Thread):
     def doA(self):
         lock.acquire()
+        # lockA.acquire()
+        print(self.name, "gotlockA", time.ctime())
+        time.sleep(3)
+        lock.acquire()
