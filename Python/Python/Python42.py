26c7582826c8128bf8d840b2ae06f2d43e01e03d
quanee <262504455@qq.com>
2018-03-28 14:33:35 +0800

    update file Python42.py

--- Python/Python/Python42.py
+++ Python/Python/Python42.py
@@ -5,3 +5,10 @@ import mmh3
 
 
 class BloomFilter(set):
+
+    def __init__(self, size, hash_count):
+        super(BloomFilter, self).__init__()
+        self.bit_array = bitarray(size)
+        self.bit_array.setall(0)
+        self.size = size
+        self.hash_count = hash_count
