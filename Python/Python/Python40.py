1fdf3b4e173b083331ff49ecb768f11a0dde8830
quanee <262504455@qq.com>
2018-04-29 14:34:36 +0800

    update file Python40.py

--- Python/Python/Python40.py
+++ Python/Python/Python40.py
@@ -8,3 +8,8 @@ print(list(x))
 
 # 连接多个列表或迭代器
 x = itertools.chain(range(3), range(4), [3, 2, 1])
+print(list(x))
+
+# 求列表或生成器中指定数目的元素不重复的所有组合
+x = itertools.combinations(range(4), 3)
+print(list(x))
