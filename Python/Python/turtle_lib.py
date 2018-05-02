a47a5e02b83bb233f8b18216f6b8ed2255f41656
quanee <262504455@qq.com>
2018-05-02 14:34:43 +0800

    update file turtle_lib.py

--- Python/Python/turtle_lib.py
+++ Python/Python/turtle_lib.py
@@ -14,3 +14,11 @@ left(angle) 向左旋转angle角度
 goto(x,y) 移动到绝对坐标（x，y）处
 setx() 将当前x轴移动到指定位置
 sety() 将当前y轴移动到指定位置
+setheading(angle) 设置当前朝向为angle角度
+home() 设置当前画笔位置为原点，朝向东
+circle() 绘制一个指定半径，角度，以及步骤的圆圈
+dot(r) 绘制一个指定直径和颜色的原点
+undo() 撤销画笔最后一步动作
+speed() 设置画笔的绘制速度，参数为0-10之间
+
+turtle颜色和字体绘制方法
