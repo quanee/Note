cc4e0b10dbeb7bec34ca9d8a27bb6a34962d0d7e
quanee <262504455@qq.com>
2018-05-14 14:35:20 +0800

    update file Python_xml.py

--- Python/leaning python/ulm/Python_xml.py
+++ Python/leaning python/ulm/Python_xml.py
@@ -30,3 +30,7 @@ class BookHandler(xml.sax.handler.ContentHandler):
 
     def startElement(self, name, attributes):
         if name == 'title':
+            self.inTitle = True
+
+    def characters(self, data):
+        if self.inTitle:
