d1a3af8fcfb6ab96abf6b45dd4108656af4df623
quanee <262504455@qq.com>
2018-05-27 14:35:39 +0800

    update file Conv_Neural_Net_Advance.py

--- Python/TensorFlow/Conv_Neural_Net_Advance.py
+++ Python/TensorFlow/Conv_Neural_Net_Advance.py
@@ -62,3 +62,10 @@ logits = tf.add(tf.matmul(local4, weight5), bias5)
 
 
 def loss(logits, labels):
+    labels = tf.cast(labels, tf.int64)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name='cross_entropy_per_example')
+    cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+    tf.add_to_collection('losses', cross_entropy_mean)
+
+    return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
