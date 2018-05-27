e1b156a4b4f0ae5db9768bcab77b0d9c6ad9bae1
quanee <262504455@qq.com>
2018-05-27 14:35:34 +0800

    update file Conv_Neural_Net_Advance.py

--- Python/TensorFlow/Conv_Neural_Net_Advance.py
+++ Python/TensorFlow/Conv_Neural_Net_Advance.py
@@ -5,3 +5,12 @@ import numpy as np
 import time
 
 
+
+# 训练轮数
+max_steps = 3000
+batch_size = 128
+# 下载CIFAR-10默认路径
+data_dir = '/tmp/cifar10_data/cifar-10-batches-bin'
+
+
+def variable_with_weight_loss(shape, stddev, wl):
