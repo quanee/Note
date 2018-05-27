ec3f61c784d38a49b27d92da43a834579e67ef88
quanee <262504455@qq.com>
2018-05-27 14:35:38 +0800

    update file Conv_Neural_Net_Advance.py

--- Python/TensorFlow/Conv_Neural_Net_Advance.py
+++ Python/TensorFlow/Conv_Neural_Net_Advance.py
@@ -45,3 +45,12 @@ bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
 conv2 = tf.nn.relu(tf.nn.bais_add(kernel2, bias2))
 norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
 pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+reshape = tf.reshape(pool2, [batch_size, -1])
+dim = reshape.get_shape()[1].value
+weight3 = variable_with_weight_loss(shape=[dim, 384], stddev=0.04, wl=0.004)
+bias3 = tf.Variable(tf.constant(0.1, shape=[384]))
+local3 = tf.nn.relu(tf.matmul(reshape, weight3) + bias3)
+
+weight4 = variable_with_weight_loss(shape=[384, 192], stddev=0.04, wl=0.004)
+bais4 = tf.Variable(tf.constant(0.1, shape=[192]))
