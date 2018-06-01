3aa1c588502af33c1fd2a2f95b69f4642677b97a
quanee <262504455@qq.com>
2018-06-01 14:35:49 +0800

    update file HDF5_2.py

--- Python/HDF5/HDF5_2.py
+++ Python/HDF5/HDF5_2.py
@@ -7,3 +7,6 @@ f.close()
 '''
 f = h5py.File("name.hdf5", "w")  # 创建或覆盖文件
 f = h5py.File("name.hdf5", "r")  # 只读打开(文件已存在)
+f = h5py.File("name.hdf5", "r+")  # 读写打开(文件已存在)
+f = h5py.File("name.hdf5", "a")  # 读写打开(不存在则创建)
+f = h5py.File("name.hdf5", "w-")  # 防止覆盖(存在则打开失败)
