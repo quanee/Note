44603ecb88326a418188e57c05b03773aed55eb5
quanee <262504455@qq.com>
2018-06-01 14:35:53 +0800

    update file HDF5_2.py

--- Python/HDF5/HDF5_2.py
+++ Python/HDF5/HDF5_2.py
@@ -22,3 +22,6 @@ print(f)
     core驱动: 将整个文件保存在内存中, 能存储的数据量有限, 超快速的读写
             f = h5py.File("name.hdf5", driver="core")
             在磁盘上创建一个"备份存储", 当内存文件映像关闭时, 其内容保存到磁盘上
+            f = h5py.File("name.hdf5", driver="core", backing_store=True)
+    family驱动: 将一个大文件分成多个大小一致的文件
+            1G chunks
