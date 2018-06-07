9849617ca5d32b297d461077df3a1a45981d7c7c
quanee <262504455@qq.com>
2018-06-07 14:36:01 +0800

    update file Python_iter.py

--- Python/leaning python/base/Python_iter.py
+++ Python/leaning python/base/Python_iter.py
@@ -22,3 +22,5 @@ print(list(enumerate('boss')))
 
 
 # 自动使用迭代协议来扫描文件 返回最高(最低)的字符串的值行
+print(max(open('Python_dict.py', 'r', encoding='utf8')), )
+print(min(open('Python_dict.py', 'r', encoding='utf8')))
