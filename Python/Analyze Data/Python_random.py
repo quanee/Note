c5127de0763f6910c55c9792ed071c8dbf3e78b3
quanee <262504455@qq.com>
2018-06-20 14:36:27 +0800

    update file Python_random.py

--- Python/Analyze Data/Python_random.py
+++ Python/Analyze Data/Python_random.py
@@ -6,3 +6,7 @@ print(random.random())
 # 返回给定范围内均匀的随机分布
 print(random.uniform(1, 9))
 print(random.randrange(20))
+print(random.randrange(0, 99, 3))
+print(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
