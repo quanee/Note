ef5e61d47b2d421467a975f134914cc2c1643d5e
quanee <262504455@qq.com>
2018-06-20 14:36:28 +0800

    update file Python_random.py

--- Python/Analyze Data/Python_random.py
+++ Python/Analyze Data/Python_random.py
@@ -13,3 +13,8 @@ items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 random.shuffle(items)
 print(items)
 print(random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 5))
+
+weighted_choices = [('Three', 3), ('Two', 2), ('One', 1), ('Four', 4)]
+
+population = [val for val, cnt in weighted_choices for i in range(cnt)]
+print(random.choice(population))
