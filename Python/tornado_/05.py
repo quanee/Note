5d4eb981688e9b8c7eaea434403d267843399b88
quanee <262504455@qq.com>
2018-06-23 14:36:36 +0800

    update file 05.py

--- Python/tornado_/05.py
+++ Python/tornado_/05.py
@@ -9,3 +9,10 @@ from tornado.options import define, options
 from tornado.web import RequestHandler, url
 
 tornado.options.define('port', type=int, default=8888, help='服务器端口')
+
+
+class IndexHandler(RequestHandler):
+    """主页处理类"""
+    def get(self):
+        """get请求方式"""
+        # self.write('<a href="'+self.reverse_url('cpp_url') + '">cpp</a>')
