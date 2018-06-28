86b40c7309eb5b86892ab9faeb8fcca6d07c7afa
quanee <262504455@qq.com>
2018-06-28 14:36:57 +0800

    update file rabbit_topic_send.py

--- Python/Rabbit/rabbit_topic_send.py
+++ Python/Rabbit/rabbit_topic_send.py
@@ -6,3 +6,5 @@ credentials = pika.PlainCredentials('pangdahia', 'moonboss')
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
 channel = connection.channel()
+
+channel.exchange_declare(exchange='topic_logs', type='topic')
