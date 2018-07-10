12f3bbbe3fcb52f56503e5cc6ef72b6e7a860a24
quanee <262504455@qq.com>
2018-07-10 14:39:55 +0800

    update file Python_numpy.py

--- Python/Analyze Data/Python_numpy.py
+++ Python/Analyze Data/Python_numpy.py
@@ -23,3 +23,27 @@ print(np.zeros(10, np.int))
 print(np.empty(10, np.int))
 
 # 创建随机数向量
+print(np.random.randn(10))
+
+
+s = 'Hello, Python!'
+print(np.fromstring(s, dtype=np.int8))
+
+
+def multiply99(i, j):
+    return (i + 1) * (j + 1)
+
+
+print(np.fromfunction(multiply99, (9, 9)))
+
+
+# 数组
+a = np.array([np.arange(3), np.arange(3)])
+print(a)
+print(a.shape)  # 形状
+print(a.ndim)  # 维度
+
+# 单位矩阵
+print(np.identity(9).astype(np.int8))
+print(a.tolist())
+print(type(a.tolist()))
