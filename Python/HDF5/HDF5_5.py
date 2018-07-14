e5cc7e2fc5bd9332f66d2c76cef90e9d285ce6a7
quanee <262504455@qq.com>
2018-07-14 14:40:06 +0800

    update file HDF5_5.py

--- Python/HDF5/HDF5_5.py
+++ Python/HDF5/HDF5_5.py
@@ -83,3 +83,46 @@ print(f['softlink'] == grp['dataset'])
 softlink = h5py.SoftLink('/some/path')
 print(softlink)
 print(softlink.path)
+
+
+grp.move('dataset', 'new_dataset_name')
+dset2 = grp.create_dataset('dataset', (50, ))
+print(f['softlink'] == dset)
+# 只识别名字
+print(f['softlink'] == dset2)
+
+# 软链接在创建时不会检查
+f['broken'] = h5py.SoftLink('/some/nonexistent/object')
+# print(f['broken'])  # KeyError: 'Unable to open object (component not found)'
+
+# 外部链接
+with h5py.File('file_with_resource.hdf5', 'w') as f1:
+    f1.create_group('mygroup')
+f1.close()
+
+f2 = h5py.File('linking_file.hdf5', 'w')
+f2['linkname'] = h5py.ExternalLink('file_with_resource.hdf5', 'mygroup')
+
+grp = f2['linkname']
+print(grp.name)
+print(grp.file)
+print(f2)
+
+# .parent指向外部文件根组
+print(f2['/linkname'].parent == f2['/'])
+# 创建时检查文件和对象名 3.X貌似不检查
+f2['anotherlink'] = h5py.ExternalLink('missing.hdf5', '/')
+
+# 对象名字注解
+grp = f.create_group(u'e_with_accent_\u00E9')
+print(grp.name)
+f.close()
+
+# 用get决定对象类型
+f = h5py.File('get_demo.hdf5', 'w')
+f.create_group('subgroup')
+f.create_dataset('dataset', (100, ))
+
+for name in f:
+    print(name, f.get(name, getclass=True))
+
