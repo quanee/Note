925957e0d7c5e585ee4bf3aee473970d072daf89
quanee <262504455@qq.com>
2018-07-17 14:40:23 +0800

    update file mytimer3.py

--- Python/leaning python/base/mytimer3.py
+++ Python/leaning python/base/mytimer3.py
@@ -12,3 +12,7 @@ timefunc = time.clock if sys.platform == 'win32' else time.time
 def timer(func, *pargs, _reps=1000, **kargs):
     trace(func, pargs, kargs, _reps)
     start = timefunc()
+
+    for i in range(_reps):
+        ret = func(*pargs, **kargs)
+    elapsed = timefunc() - start
