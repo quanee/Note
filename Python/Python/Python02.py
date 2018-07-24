c9015cf1534b51c8f135da06a7ee64d30c2e18e9
quanee <262504455@qq.com>
2018-07-24 14:41:12 +0800

    update file Python02.py

--- Python/Python/Python02.py
+++ Python/Python/Python02.py
@@ -103,3 +103,18 @@ print(time.ctime())
 # 当前时间
 now_time = datetime.datetime.now()
 print(now_time)
+# 昨天的现在
+yesterday = now_time + datetime.timedelta(days=-1)
+print(yesterday)
+# 现在的前一秒
+now_old = now_time + datetime.timedelta(seconds=-1)
+print(now_old)
+
+
+
+
+"""
+== 判断值相等
+is 判断引用相等
+"""
+a = [1, 2, 3]
