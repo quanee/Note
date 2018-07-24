93ac06bff84707ed4ca6b07f0ffdd99e8e67e848
quanee <262504455@qq.com>
2018-07-24 14:41:08 +0800

    update file Python02.py

--- Python/Python/Python02.py
+++ Python/Python/Python02.py
@@ -13,3 +13,17 @@ import datetime
 # sys.path　　　　---程序中导入模块对应的文件必须放在sys.path包含的目录中，使用sys.path.append添加自己的模块路径
 # sys.modules　　---This is a dictionary that maps module names to modules which have already been loaded
 # sys.stdin,sys.stdout,sys.stderr　　---包含与标准I/O 流对应的流对象
+
+
+# os模块　　该模块包含普遍的操作系统功能
+# os.name字符串指示你正在使用的平台。比如对于Windows，它是'nt'，而对于Linux/Unix用户，它是'posix'
+# os.getcwd()函数得到当前工作目录，即当前Python脚本工作的目录路径
+# os.getenv()和os.putenv()函数分别用来读取和设置环境变量
+# os.listdir()返回指定目录下的所有文件和目录名
+# os.remove()函数用来删除一个文件
+# os.system()函数用来运行shell命令
+# os.linesep字符串给出当前平台使用的行终止符。例如，Windows使用' '，Linux使用' '而Mac使用' '
+# os.sep 操作系统特定的路径分割符
+# os.path.split()函数返回一个路径的目录名和文件名
+# os.path.isfile()和os.path.isdir()函数分别检验给出的路径是一个文件还是目录
+# os.path.existe()函数用来检验给出的路径是否真地存在
