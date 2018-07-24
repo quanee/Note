63b3579d1c8d92f6fa4ab70845c06ebd18d196b3
quanee <262504455@qq.com>
2018-07-24 14:41:15 +0800

    update file Python02.py

--- Python/Python/Python02.py
+++ Python/Python/Python02.py
@@ -162,3 +162,19 @@ t.setNum(50)
 print(t.getNum())
 t.num = 200
 print(t.num)
+
+
+class Money(object):
+    def __init__(self):
+        self.__money = 0
+
+    @property
+    def money(self):
+        return self.__money
+
+    @money.setter
+    def money(self, value):
+        if isinstance(value, int):
+            self.__money = value
+        else:
+            print("Error:Not Int")
