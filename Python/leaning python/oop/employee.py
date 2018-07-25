141b4fcd7b7684ec66870d327b116530d213e2c4
quanee <262504455@qq.com>
2018-07-25 14:41:16 +0800

    update file employee.py

--- Python/leaning python/oop/employee.py
+++ Python/leaning python/oop/employee.py
@@ -2,3 +2,9 @@ class Employee:
     def __init__(self, name, salary=0):
         self.name = name
         self.salary = salary
+
+    def giveRaise(self, percent):
+        self.salary = self.salary + (self.salary * percent)
+
+    def work(self):
+        print(self.name, "does stuff")
