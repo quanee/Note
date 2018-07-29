36728cdaad62ec93b9a18a3badc727ebd7be8928
quanee <262504455@qq.com>
2018-07-29 14:41:41 +0800

    update file boids.py

---
+++ Python/python playground/boids/boids.py
@@ -0,0 +1,16 @@
+import sys
+import argparse
+import math
+import numpy as np
+import matplotlib.pylab as plt
+import matplotlib.animation as animation
+from scipy.spatial.distance import squareform
+from scipy.spatial.distance import pdist
+from scipy.spatial.distance import cdist
+from numpy.linalg import norm
+"""模拟鸟群"""
+
+
+
+width, height = 1000, 1000
+
