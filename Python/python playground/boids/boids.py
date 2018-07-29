59042d5ede406239a2052852e21e0a1ae0842ecb
quanee <262504455@qq.com>
2018-07-29 14:41:42 +0800

    update file boids.py

--- Python/python playground/boids/boids.py
+++ Python/python playground/boids/boids.py
@@ -14,3 +14,20 @@ from numpy.linalg import norm
 
 width, height = 1000, 1000
 
+
+class Boids:
+    """class that represents Boids simulation"""
+
+    def __init__(self, N):
+        """initialize the Boid simulation"""
+        self.pos = [width / 2.0, height / 2.0] + 10 * np.random.rand(2 * N).reshape(N, 2)
+        # normalized random velocaties
+        angles = 2 * math.pi * np.random.rand(N)
+        self.vel = np.array(list(zip(np.sin(angles), np.cos(angles))))
+        self.N = N
+        # minimum destance of approach
+        self.minDist = 25.0
+        # minimum magnitude of velocities calculated by "rules"
+        self.maxRuleVel = 0.03
+        # maximum maginit of the final velocity
+        self.maxVel = 2.0
