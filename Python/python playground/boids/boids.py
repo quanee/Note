f276afa0122bbf581b22520b98c9a3d066b83856
quanee <262504455@qq.com>
2018-07-29 14:41:42 +0800

    update file boids.py

--- Python/python playground/boids/boids.py
+++ Python/python playground/boids/boids.py
@@ -31,3 +31,21 @@ class Boids:
         self.maxRuleVel = 0.03
         # maximum maginit of the final velocity
         self.maxVel = 2.0
+
+    def tick(self, frameNum, pts, beak):
+        """Update the simulation by one time step."""
+        # get pairWise distances
+        self.distMatrix = squareform(pdist(self.pos))
+        # apply rules:
+        self.vel += self.applyRules()
+        self.limit(self.vel, self.maxVel)
+        self.pos += self.vel
+        self.applyBC()
+        # update data
+        pts.set_data(self.pos.reshape(2 * self.N)[::2], self.pos.reshape(2 * self.N)[1::2])
+        vec = self.pos + 10 * self.vel / self.maxVel
+        beak.set_data(vec.reshape(2 * self.N)[::2], vec.reshape(2 * self.N)[1::2])
+
+    def limitVec(self, vec, maxVal):
+        """limit the magnitide of the 2D vector"""
+        mag = norm(vec)
