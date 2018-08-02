7323c22a13cd0d2083e57a95b34013761e68ab4f
quanee <262504455@qq.com>
2018-08-02 14:42:09 +0800

    update file rabbit_fanout_recv.py

--- Python/Rabbit/rabbit_fanout_recv.py
+++ Python/Rabbit/rabbit_fanout_recv.py
@@ -15,3 +15,10 @@ print('queue', queue_name)
 channel.queue_bind(exchange='logs', queue=queue_name)
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+
+# channel.basic_consume(callback, queue=queue_name, no_ack=True)
