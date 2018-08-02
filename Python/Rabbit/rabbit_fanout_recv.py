f9632476af54d325417bc00b7ded3cc23f735949
quanee <262504455@qq.com>
2018-08-02 14:42:09 +0800

    update file rabbit_fanout_recv.py

--- Python/Rabbit/rabbit_fanout_recv.py
+++ Python/Rabbit/rabbit_fanout_recv.py
@@ -10,3 +10,8 @@ channel.exchange_declare(exchange='logs', type='fanout')
 # 不指定queue名字,rabbit会随机分配一个名字,exclusive=True(唯一排他)会在使用此queue的消费者断开后,自动将queue删除
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
+print('queue', queue_name)
+
+channel.queue_bind(exchange='logs', queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
