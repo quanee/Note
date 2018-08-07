0f64dd3cacf7276346a9f968271b39e1cb2699e4
quanee <262504455@qq.com>
2018-08-07 14:42:31 +0800

    update file Python_decorator.py

--- Python/leaning python/ulm/Python_decorator.py
+++ Python/leaning python/ulm/Python_decorator.py
@@ -66,3 +66,37 @@ class tracer:
 @tracer
 def spam(a, b, c):
     print(a + b + c)
+
+
+@tracer
+def eggs(x, y):
+    print(x ** y)
+
+
+spam(1, 2, 3)
+spam(a=4, b=5, c=6)
+
+eggs(2, 16)
+eggs(4, y=4)
+
+
+'''使用嵌套函数来装饰方法'''
+def tracer(func):
+    calls = 0
+
+    def onCall(*args, **kwargs):
+        nonlocal calls
+        calls += 1
+        print('call %s to %s' % (calls, func.__name__))
+
+        return func(*args, **kwargs)
+    return onCall
+
+
+@tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+print('#' * 50)
+spam(1, 2, 3)
