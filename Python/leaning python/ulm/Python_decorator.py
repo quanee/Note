4b1dc178882efa521d9c918dcb8c00e2f5be2207
quanee <262504455@qq.com>
2018-08-07 14:42:34 +0800

    update file Python_decorator.py

--- Python/leaning python/ulm/Python_decorator.py
+++ Python/leaning python/ulm/Python_decorator.py
@@ -202,3 +202,37 @@ class timer:
         result = self.func(*args, **kwargs)
         elapsed = time.clock() - start
         self.alltime += elapsed
+        print('%s: %.5f, %.5f' % (self.func.__name__, elapsed, self.alltime))
+        return result
+
+
+@timer
+def listcomp(N):
+    return [x * 2 for x in range(N)]
+
+
+@timer
+def mapcall(N):
+    return list(map((lambda x: x * 2), range(N)))
+
+
+result = listcomp(5)
+listcomp(50000)
+listcomp(500000)
+listcomp(1000000)
+print(result)
+print('allTime = %s' % listcomp.alltime)
+print('')
+result = mapcall(5)
+mapcall(50000)
+mapcall(500000)
+mapcall(1000000)
+print(result)
+print('allTime = %s' % mapcall.alltime)
+
+print('map/comp = %s' % round(mapcall.alltime / listcomp.alltime, 3))
+
+
+def timer(label='', trace=True):
+    class Timer:
+        def __init__(self, func):
