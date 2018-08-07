d07562ccf1129ac3c796027861290d7e09a5d637
quanee <262504455@qq.com>
2018-08-07 14:42:35 +0800

    update file Python_decorator.py

--- Python/leaning python/ulm/Python_decorator.py
+++ Python/leaning python/ulm/Python_decorator.py
@@ -236,3 +236,37 @@ print('map/comp = %s' % round(mapcall.alltime / listcomp.alltime, 3))
 def timer(label='', trace=True):
     class Timer:
         def __init__(self, func):
+            self.func = func
+            self.alltime = 0
+
+        def __call__(self, *args, **kwargs):
+            start = time.clock()
+            result = self.func(*args, **kwargs)
+            elapsed = time.clock() - start
+            self.alltime += elapsed
+            if trace:
+                formats = '%s %s: %.5f, %.5f'
+                values = (label, self.func.__name__, elapsed, self.alltime)
+                print(formats % values)
+            return result
+    return Timer
+
+
+@timer(label='[CCC]==>')
+def listcomp(N):
+    return [x * 2 for x in range(N)]
+
+
+@timer(trace=True, label='[MMM]==>')
+def mapcall(N):
+    return list(map((lambda x: x * 2), range(N)))
+
+
+for func in (listcomp, mapcall):
+    print('')
+    result = func(5)
+    func(50000)
+    func(500000)
+    func(1000000)
+    print(result)
+    print('allTime = %s' % func.alltime)
