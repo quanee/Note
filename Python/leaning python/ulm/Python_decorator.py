842d05edca7135686b6bfecbb5409ea94da77e4e
quanee <262504455@qq.com>
2018-08-07 14:42:30 +0800

    update file Python_decorator.py

--- Python/leaning python/ulm/Python_decorator.py
+++ Python/leaning python/ulm/Python_decorator.py
@@ -32,3 +32,37 @@ print(x.attr)
 class tracer:
     def __init__(self, func):
         self.calls = 0
+        self.func = func
+
+    def __call__(self, *args):
+        self.calls += 1
+        print('call %s to %s' % (self.calls, self.func.__name__))
+        self.func(*args)
+
+
+@tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+print('#' * 50)
+spam(1, 2, 3)
+spam('a', 'b', 'c')
+print(spam.calls)  # spam实际为tracer类的一个实例
+
+
+'''类实例属性'''
+class tracer:
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        print('call %s to %s' % (self.calls, self.func.__name__))
+        self.func(*args, **kwargs)
+
+
+@tracer
+def spam(a, b, c):
+    print(a + b + c)
